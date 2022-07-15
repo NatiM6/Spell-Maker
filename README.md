b2fc2cff8f70b387e4942d44accff2cbc3c11a20
NatiM6 <natanekm@gmail.com>
2022-07-15 22:10:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpellMaker
+ Spell-drawing game
---
+++ desktop/src/com/github/natim6/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.github.natim6;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.natim6.SpellMaker;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Spell Maker");
+		config.setWindowedMode(800, 600);
+		new Lwjgl3Application(new SpellMaker(), config);
+	}
+}
